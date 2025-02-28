6866704923b1467d9425fc7a27abd8857baf4af0
N4GR <107074986+N4GR@users.noreply.github.com>
2025-02-28 05:01:57 +0000

    UPDATE | Thumbnailer template.

---
+++ src/thumbnailer.py
@@ -0,0 +1,11 @@
+from src.imports import *
+
+class Thumbnailer:
+    def __init__(self):
+        pass
+    
+    def get_thumbnail(
+            self,
+            img_dir: str
+    ) -> None:
+        pass
